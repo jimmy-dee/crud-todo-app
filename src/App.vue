878463aaec84c6e07746fce5850c3cc9ff2f3dcb
James Davies <jdavies@nrl.com.au>
2020-03-05 07:51:26 +1100

    Cleaned up vue cli template in preperation for dev work

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+  <div id="todo-app">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -9,7 +8,7 @@
 import HelloWorld from './components/HelloWorld.vue';
 
 export default {
-  name: 'App',
+  name: 'todo-app',
   components: {
     HelloWorld,
   },
