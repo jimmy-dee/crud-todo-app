45205d5ff1cbb852aa33dd63baac119ab877a216
James Davies <jdavies@nrl.com.au>
2020-03-09 00:20:51 +1100

    Fleshed out html and styles for todo app

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,131 @@
 <template>
-  <div id="todo-app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main class="l-content">
+    <div class="todo u-flex u-flex--center u-flex--column u-spacing-m-small u-spacing-p-small">
+      <h1 class="">Todo list</h1>
+      <form class="todo-create">
+        <input
+          class="todo-create__input"
+          placeholder="Create a new todo"
+          type="text"
+        />
+        <button
+          aria-label="Create a new todo"
+          class="todo-create__button"
+          v-on:click="createTodo"
+        >
+          +
+        </button>
+      </form>
+      <ul
+        aria-live="polite"
+        class="todo-list"
+      >
+        <li
+          class="todo-list__item"
+          v-bind:key="todo.id"
+          v-for="todo in todos"
+        >
+          <input
+            type="checkbox"
+            v-bind:aria-checked="todo.completed"
+            v-bind:id="'todo' + todo.id"
+            v-bind:value="todo.id"
+            v-model="todo.completed"
+            v-on:change="toggleTodo(todo)"
+          />
+          <label v-bind:for="'todo' + todo.id">
+            <component
+              class=""
+              v-bind:is="todo.completed ? 'del' : 'span'"
+            >
+              {{ todo.title }}
+            </component>
+          </label>
+          <button
+            v-bind:aria-label="'remove todo:' + todo.title"
+            v-on:click="removeTodo(todo)"
+          >
+            X
+          </button>
+        </li>
+      </ul>
+    </div>
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+// import HelloWorld from './components/HelloWorld.vue';
+import axios from 'axios';
 
 export default {
   name: 'todo-app',
   components: {
-    HelloWorld,
+    // HelloWorld,
+  },
+  data() {
+    return {
+      baseApiUrl: 'https://jsonplaceholder.typicode.com/todos',
+      todos: null,
+      userId: 2,
+    };
+  },
+  computed: {
+    apiUrl() {
+      return `${this.baseApiUrl}?userId=${this.userId}`;
+    },
+  },
+  methods: {
+    createTodo(newTodo) {
+      axios.post(this.baseApiUrl, { ...newTodo, userId: this.userId })
+        .then(() => {
+          // this is what i would do if the api was returning real data
+          // this.todos = this.fetchTodos();
+
+          // mock created
+          this.todos.push({ ...newTodo, userId: this.userId });
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    fetchTodos() {
+      axios.get(this.apiUrl)
+        .then((response) => {
+          this.todos = response.data;
+        })
+        .catch((error) => {
+          // handle error
+          console.log(error);
+        })
+        .then(() => {
+          // always executed
+        });
+    },
+    removeTodo(removedTodo) {
+      axios.delete(`${this.baseApiUrl}/${removedTodo.id}`, removedTodo)
+        .then(() => {
+          // this is what i would do if the api was returning real data
+          // this.todos = this.fetchTodos();
+
+          // mock deletion
+          this.todos = this.todos.filter((todo) => todo.id !== removedTodo.id);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    toggleTodo(modifiedTodo) {
+      axios.put(`${this.baseApiUrl}/${modifiedTodo.id}`, modifiedTodo)
+        .then(() => {
+          this.todos = this.fetchTodos();
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+  created() {
+    this.fetchTodos();
   },
 };
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
